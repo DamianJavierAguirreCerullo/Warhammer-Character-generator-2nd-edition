10bdb61e13794b09dcdbcf9a90b2047d3e5c948d
DamianJavierAguirreCerullo <djac441@gmail.com>
2023-08-04 17:02:47 -0300

    Casi se termina la creacion de personajes del elfo no se encontro bug pero se sabe que si en vez de numeros se digita un caracter o varios se rompe y sale automaticamente

--- Warhammer Character generator 2nd edition/Program.cs
+++ Warhammer Character generator 2nd edition/Program.cs
@@ -6,7 +6,9 @@ System.Random random = new Random();
 int valor_un_de_diez = 0;
 int valor_dos_de_diez = 0;
 int valor_un_de_cien = 0;
+int leer_seleccion = 0;
 string switchControl = "menu";
+
 
 int habilidad_de_armas = 0;
 int habilidad_de_proyectil = 0;
@@ -25,6 +27,9 @@ int magia = 0;
 int puntos_de_locura = 0;
 int puntos_de_destino = 0;
 
+string nombre = "";
+string profesion = "";
+
 
 List<string> elfo_habilidades = new List<string>();
 List<string> enano_habilidades = new List<string>();
@@ -38,19 +43,35 @@ List<string> humano_talentos = new List<string>();
 
 List<string> elfo_profesion_inicial = new List<string>
 {
-    "Aprendiz de hechicero","Forajido","Escolta","Bribón","Escriba","Artista","Marinero","Embajador","Estudiante","Ladrón","Cazador","Menestral","Guerrero de camarilla","Vagabundo","Mercenario","Mensajero"
+    "Aprendiz de hechicero","Artista","Embajador","Cazador","Guerrero de camarilla","Mercenario","Mensajero","Forajido","Escolta","Bribón","Escriba","Marinero","Estudiante","Ladrón","Menestral","Vagabundo"
+};
+List<string> elfo_profesion_inicial_descripcion = new List<string>
+{
+    "Los hymanos que nacen con capacidades magicas son individuos pelogrosos y temibles. Un hechicero inexperto siempre acaba rodeado de demonios y catastrofes.\n" +
+    "Para acabar con esta amenaza, el imperio envia lejos a estas personas para que se unan a una de las ocho ordenes de hechiceria.\n" +
+    "Durante su aprendizaje los jovenes hechiceros aprenden a usar la magia sin percances, y deciden a cual de las ordenes se unira.\n" +
+    "Algunos aprendices estaran obligados a servir al hechicero que descubrio sus talentos,\n" +
+    "mientra que otros se van de aventuras para ganar dinero suficiente para pagar su tutela en los Colegios de la Magia.\n" +
+    "Los elfos son magicos por naturaleza, y no necesitan asistir a estas instituciones humanas, sino que aprenden de sus propios maestros del saber sobre las ordenes de la magia\n" +
+    "Esquema de mejoras Agilidad +5 Inteligencia +10 Voluntad +15 Heridas +2 Magia +1\n" +
+    "Habilidades:Buscar,Canalizacion,Hablar idioma(clasico),leer/escribir,Lengua arcana(Magia),Percepcion,Sabiduria academica(Magia),Sentir magia\n" +
+    "Talentos: Afinidad con el Aethyr o Manos rapidas, Intelectual o Muy resistente,Magia pueril(Arcana)\n" +
+    "Enseres: Baculo,mochila,libro impreso\n" +
+    "accesos:Erudito,escriba,estudiante,hechicero vulgar\n" +
+    "salidas:Erudito,escriba,hechicero adepto\n" +
+    "nota: si quieres ser cvapaz de lanzar hechizos debes aumentar tu caracteristica de Magia con la mejora gratis disponiblñe durante la creacion de personaje.Los halflings y enanos no pueden acceder a esta profesion. Los hechiceros son temidos y a veces odiados. Piensatelo bien antes de escoger esta profesion.",
 };
 List<string> enano_profesion_inicial = new List<string>
 {
-    "Noble","Alborotador","Forajido","Guardaespaldas","Gladiador","Sicario","Cazarratas","Burgués","Portador de runas","Cochero","Escriba","Artista","Marinero","Sirviente","Rompescudos","Contrabandista","Soldado","Estudiante","Ladrón","Cazador","Peajero","Carcelero","Saqueador de tumbas","Menestral","Matatrolls","Guardia marina","Mercenario","Vigilante","Miliciano","Minero"
+    "Alborotador","Guardaespaldas","Burgués","Cochero","Artista","Cazador","Carcelero","Guardia marina","Mercenario","Miliciano","Minero","Noble","Forajido","Gladiador","Sicario","Cazarratas","Portador de runas","Escriba","Marinero","Sirviente","Rompescudos","Contrabandista","Soldado","Estudiante","Ladrón","Peajero","Saqueador de tumbas","Menestral","Matatrolls","Vigilante"
 };
 List<string> halfling_profesion_inicial = new List<string>
 {
-    "Alborotador","Forajido","Barbero cirujano","Campesino","Osamentero","Cazarrecompensas","Cazarratas","Burgués","Miembro de séquito","Bribón","Carbonero","Artista","Sirviente","Barquero","Contrabandista","Patrulla fronteriza","Soldado","Pescador","Ladrón de tumbas","Estudiante","Ladrón","Cazador","Peajero","Saqueador de tumbas","Menestral","Vagabundo","Mercenario","Ayuda de cámara","Mensajero","Vigilante","Miliciano"
+    "Alborotador","Barbero cirujano","Osamentero","Cazarrecompensas","Burgués","Miembro de séquito","Carbonero","Artista","Barquero","Patrulla fronteriza","Pescador","Ladrón de tumbas","Cazador","Mercenario","Mensajero","Miliciano","Forajido","Campesino","Cazarratas","Bribón","Sirviente","Contrabandista","Soldado","Estudiante","Ladrón","Peajero","Saqueador de tumbas","Menestral","Vagabundo","Ayuda de cámara","Vigilante"
 };
 List<string> humano_profesion_inicial = new List<string>
 {
-    "Noble","Alborotador","Aprendiz de hechicero","Bersérker nórdico","Alguacil","Forajido","Barbero cirujano","Escolta","Batelero","Campesino","Guardaespaldas","Gladiador","Osamentero","Sicario","Cazarrecompensas","Cazarratas","Burgués","Patrulla de caminos","Miembro de séquito","Bribón","Carbonero","Cochero","Escriba","Artista","Marinero","Sirviente","Espadachín estaliano","Barquero","Contrabandista","Soldado","Pescador","Escudero","Ladrón de tumbas","Estudiante","Hechicero vulgar","Ladrón","Cazador","Matón","Iniciado","Peajero","Carcelero","Saqueador de tumbas","Kossar kislevita","Menestral","Guardia marina","Vagabundo","Mercenario","Ayuda de cámara","Mensajero","Vigilante","Miliciano","Leñador","Minero","Fanático"
+    "Alborotador","Aprendiz de hechicero","Alguacil","Barbero cirujano","Batelero","Guardaespaldas","Osamentero","Cazarrecompensas","Burgués","Miembro de séquito","Carbonero","Cochero","Artista","Espadachín estaliano","Barquero","Pescador","Ladrón de tumbas","Hechicero vulgar","Cazador","Iniciado","Carcelero","Kossar kislevita","Guardia marina","Mercenario","Mensajero","Miliciano","Minero","Noble","Bersérker nórdico","Forajido","Escolta","Campesino","Gladiador","Sicario","Cazarratas","Patrulla de caminos","Bribón","Escriba","Marinero","Sirviente","Contrabandista","Soldado","Escudero","Estudiante","Ladrón","Matón","Peajero","Saqueador de tumbas","Menestral","Vagabundo","Ayuda de cámara","Vigilante","Leñador","Fanático"
 };
 
 List<string> telentos_aleatorios = new List<string>
@@ -58,6 +79,64 @@ List<string> telentos_aleatorios = new List<string>
     "Ambidiestro", "Cortés", "Genio aritmético", "Guerrero nato", "Imperturbable", "Imitador", "Intelectual", "Muy fuerte", "Muy resistente", "Oído aguzado", "Pies ligeros", "Puntería", "Recio", "Reflejos rápidos", "Resistencia a enfermedades", "Resistencia a la magia", "Resistencia a venenos", "Robusto", "Sangre fría", "Sexto sentido", "Suerte", "Vista excelente", "Visión nocturna"
 };
 
+List<string> elfo_nombre_elementos_1 = new List<string>
+{
+    "Aed","Ael","Aelf","Aen","Aeth","Alth","An","And","Ar","Arg","Ast","Ath","Ay","Ban","Bel","Beth","Cad","Cael","Caem","Caeth","Cal","Cam","Cel","Cir","Eld","El","Elth","End","Er","Ers","Fand","Ferg","Fim","Fin","Gal","Gald","Gaen","Gaes","Ged","Gen","Ges","Geth","Glor","Has","Hath","Hel","Heth","Ill","Ind","Ist","Ith","Iy","Kor","Kyr","Ky","La","Lan","Lil","Lim","Lith","Loth","Mal","Mar","Mas","Math","Me","Mes","Men","Meth","Mor","Mort","Nal","Nar","Nen","Nor","Norl","Ri","Riabb","Riann","Rid","Rien","Riell","Ruth","Ryn","Tab","Tal","Tan","Tar","Teth","Tel","Tor","Ty","Ul","Um","Ur","Yr","Yv"
+};
+
+List<string> elfo_nombre_elementos_2 = new List<string>
+{
+    "Baen","Baine","Baire","Bar","Bhir","Brier","Brior","Brin","Daen","Daine","Daire","Dar","Dhil","Dhir","Drel","Drir","Dror","Eorl","Eos","Eoth","Eor","Fil","Fin","Fir","Hil","Hin","Hir","Hor","Il","In","Ion","Ir","Is","Ith","Laen","Laer","Laine","Laire","Lan","Las","Len","Les","Lil","Lin","Lis","Lor","Los","Maen","Mael","Mair","Mal","Mar","Mil","Min","Mir","Nael","Naen","Naer","Nail","Nair","Nal","Nan","Nar","Nael","Naen","Naer","Nail","Nair","Nal","Nan","Nar","Neael","Nean","Neal","Near","Neael","Nean","Neal","Near","Nin","Nil","Nir","Nis","Nin","Nil","Nir","Nis","Ran","Rea","Rel","Ril","Riol","Rir","Rior","Rion","Riorl","Ryel","Riorn","Rir","Rior","Rion","Riorl","Ryel","Riorn","Taen","Tain","Tair","Than","Thar","Thel","Thil","Thin","Thir","Thil","Thin","Thir","Thr","Thwe","Thrin","Thril","Thr","Thwe","Thrin","Thril","Thrin","Til","Tin","Tis","We","Yan"
+};
+
+List<string> elfo_nombre_conectores = new List<string>
+{
+    "A","Al","An","Ar","As","E","El","En","Er","Es","Fan","Fen","Fin","I","Il","In","Ir","Is","O","Ol","On","Or","Os","Ra","Ral","Ran","Rel","Ren","Ri","Ril","Rin","Ro","Rol","Ron","Ry","Sa","Sal","San","Sen","Sel","Si","Sin","So","Sol","Son","U","Ul"
+};
+
+List<string> elfo_nombre_elementos_elfa = new List<string>
+{
+    "A","Aine","Am","Ann","Arma","Arna","Arth","Ath","Beann","Bet","Beth","Brim","Brys","Deann","Det","Derth","Drian","Driel","Drys","Eann","Eanna","Earna","Earth","Elle","Eth","Eys","Eyth","Felle","Fionn","Flys","Fyll","Fyr","Fys","Illa","Ina","Ira","I","Itt","Ithi","Lielle","Lieth","Let","Leth","Lyann","Lam","Lana","Larna","La","Leann","Leath","Lel","Lelle","Leth","Let","Lyann","Lam","Lana","Larna","La","Leann","Leath","Lel","Lelle","Nelle","Nem","Neth","Niell","Niella","Ni","Nith","Nell","Nem","Neth","Niell","Niella","Ni","Nith","Ris","Rith","Rys","Ras","Reann","Rell","Relle","Rielle","Ri","Ris","Rith","Rys","Ras","Reann","Rell","Relle","Rielle","Ri","Sar","Sath","Ser","Seth","Sir","Sith","Sor","Soth","Sher","Shar","Shir","Sys","Teal","Teann","Ter","Tha","Ther","Thi","Ther","Thryn","Thyn","Tir","Tor","Tos","Tryan","Trys","Yll","Yrs","Ys"
+};
+
+
+void ElfoNombre(int genero)
+{
+    string name = "";
+    if (genero == 1) 
+    { 
+        name = elfo_nombre_elementos_1[(random.Next(0, elfo_nombre_elementos_1.Count))]+
+           elfo_nombre_conectores[(random.Next(0, elfo_nombre_conectores.Count))]+
+           elfo_nombre_elementos_2[(random.Next(elfo_nombre_elementos_2.Count))];
+    }
+    else if (genero == 2)
+    {
+        name = elfo_nombre_elementos_1[(random.Next(0, elfo_nombre_elementos_1.Count))] +
+           elfo_nombre_conectores[(random.Next(0, elfo_nombre_conectores.Count))] +
+           elfo_nombre_elementos_2[(random.Next(elfo_nombre_elementos_elfa.Count))];
+    }
+    nombre = name;
+}
+
+void ElfoProfesion(int numeroProfesion)
+{
+    string prof = "";
+    int num = 0;
+    prof = elfo_profesion_inicial[numeroProfesion];
+    Console.WriteLine($"\n{prof}\n" +
+        $"{elfo_profesion_inicial_descripcion[numeroProfesion]}");
+
+    profesion = prof;
+
+    //if(num == 1) 
+    //{
+    //    profesion = prof;
+    //}
+    //else if(num == 2)
+    //{
+    //    return;
+    //}
+}
 int UnDeDiez()
 {
     valor_dos_de_diez = random.Next(1, 10);
@@ -74,6 +153,20 @@ int UnDeCien()
 {
     valor_un_de_cien = random.Next(1, 100);
     return valor_un_de_cien;
+}
+
+void Mostrar()
+{
+    Console.WriteLine($"{ habilidad_de_armas},{ habilidad_de_proyectil},{agilidad},{ inteligencia},{ voluntad},{ empatia},{ movimiento},{ heridas},{ fuerza},{ bonificacion_por_fuerza},{ resistencia},{ bonificacion_por_resistencia},{puntos_de_destino}");
+    foreach (var item in elfo_talentos)
+    {
+        Console.WriteLine(item);
+    }
+    foreach (var item in elfo_habilidades)
+    {
+        Console.WriteLine(item);
+    }
+    Console.WriteLine($"{nombre},{profesion}");
 }
 
 void BonificadoresFuerzaYResistencia()
@@ -94,7 +187,7 @@ void EstadisticasAleatorias()
     empatia = DosDeDiez();
 }
 
-void EstadisticasAleatoriasElfo()
+void GeneradorElfo()
 {
     EstadisticasAleatorias();
     habilidad_de_armas += 20;
@@ -107,8 +200,8 @@ void EstadisticasAleatoriasElfo()
     empatia += 20;
     movimiento = 3;
     BonificadoresFuerzaYResistencia();
+    
     UnDeDiez();
-
     if (valor_un_de_diez <= 3)
     {
         heridas = 9;
@@ -143,17 +236,63 @@ void EstadisticasAleatoriasElfo()
     elfo_habilidades.Add("Hablar idioma(Eltharin)");
     elfo_habilidades.Add("Hablar idioma(Reikspiel)");
     elfo_habilidades.Add("Sabiduría popular(Elfos)");
+    elfo_talentos.Add("Vision nocturna");
+    elfo_talentos.Add("Vista exelente");
+
+    Console.WriteLine("Elija uno de los siguientes talentos para su elfo:\n" +
+        "Afinidad con el Aethyr o Especialista en armas (arco largo)\n" +
+        "Afinidad con el Aethyr +10 a sentir magia y a canalizar\n" +
+        "Especialista en armas (arco largo) puedes usar el arco largo sin penalizacion\n" +
+        "Escriba 1 y enter para Afinidad con el aethyr o 2 para especialista en armas(arco largo)");
+    leer_seleccion = int.Parse(Console.ReadLine());
+    if (leer_seleccion == 1)
+    {
+        elfo_talentos.Add("Afinidad con el Aethyr");
+    }
+    else if(leer_seleccion == 2)
+    {
+        elfo_talentos.Add("Especialista en armas(arco largo)");
+    }
+
+    Console.WriteLine("Elija uno de los siguientes talentos para su elfo:\n" +
+        "Sangre fria o Intelectual\n" +
+        "Sangre fria +5 permanente a tu voluntad\n" +
+        "Intelectual +5 permanente a tu inteligencia\n"+
+        "Escriba 1 y enter para Sangre fria o 2 para Intelectual\n");
+
+    leer_seleccion = int.Parse(Console.ReadLine());
+        if (leer_seleccion == 1)
+        {
+            elfo_talentos.Add("Sangre fria");
+            voluntad += 5;
+        }
+        else if(leer_seleccion == 2)
+        {
+            elfo_talentos.Add("Intelectual");
+            inteligencia += 5;
+    
+        }
+
+    Console.WriteLine("Eliga un genero para su personaje\n" +
+        "Escriba 1 si desea que sea hombre y 2 si desea que sea mujer");
+    ElfoNombre(int.Parse(Console.ReadLine()));
+
+    leer_seleccion = int.Parse(Console.ReadLine());
+    ElfoProfesion(leer_seleccion);
+
+    Mostrar();
 }
-void EstadisticasAleatoriasEnano()
+void GeneradorEnano()
 {
-    habilidad_de_armas = 30+DosDeDiez();
-    habilidad_de_proyectil = 20+DosDeDiez();
-    fuerza = 20+DosDeDiez();
-    resistencia = 30+DosDeDiez();
-    agilidad = 10+DosDeDiez();
-    inteligencia = 20+DosDeDiez();
-    voluntad = 20+DosDeDiez();
-    empatia = 10+DosDeDiez();
+    EstadisticasAleatorias();
+    habilidad_de_armas += 30;
+    habilidad_de_proyectil += 20;
+    fuerza += 20;
+    resistencia += 30;
+    agilidad += 10;
+    inteligencia += 20;
+    voluntad += 20;
+    empatia += 10;
     movimiento = 5;
     BonificadoresFuerzaYResistencia();
     UnDeDiez();
@@ -185,16 +324,17 @@ void EstadisticasAleatoriasEnano()
         puntos_de_destino = 3;
     }
 }
-void EstadisticasAleatoriasHalfling()
+void GeneradorHalfling()
 {
-    habilidad_de_armas = 10+DosDeDiez();
-    habilidad_de_proyectil = 30+DosDeDiez();
-    fuerza = 10+DosDeDiez();
-    resistencia = 10+DosDeDiez();
-    agilidad = 30+DosDeDiez();
-    inteligencia = 20+DosDeDiez();
-    voluntad = 20+DosDeDiez();
-    empatia = 30+DosDeDiez();
+    EstadisticasAleatorias();
+    habilidad_de_armas += 10;
+    habilidad_de_proyectil += 30;
+    fuerza += 10;
+    resistencia += 10;
+    agilidad += 30;
+    inteligencia += 20;
+    voluntad += 20;
+    empatia += 30;
     movimiento = 4;
     BonificadoresFuerzaYResistencia();
     UnDeDiez();
@@ -226,16 +366,17 @@ void EstadisticasAleatoriasHalfling()
         puntos_de_destino = 3;
     }
 }
-void EstadisticasAleatoriasHumano()
+void GeneradorHumano()
 {
-    habilidad_de_armas = 20+DosDeDiez();
-    habilidad_de_proyectil = 20+DosDeDiez();
-    fuerza = 20+DosDeDiez();
-    resistencia = 20+DosDeDiez();
-    agilidad = 20+DosDeDiez();
-    inteligencia = 20+DosDeDiez();
-    voluntad = 20+DosDeDiez();
-    empatia = 20+DosDeDiez();
+    EstadisticasAleatorias();
+    habilidad_de_armas += 20;
+    habilidad_de_proyectil += 20;
+    fuerza += 20;
+    resistencia += 20;
+    agilidad += 20;
+    inteligencia += 20;
+    voluntad += 20;
+    empatia += 20;
     movimiento = 4;
     BonificadoresFuerzaYResistencia();
     UnDeDiez();
@@ -269,6 +410,11 @@ void EstadisticasAleatoriasHumano()
 
 }
 
+void GeneradorAleatorio()
+{
+
+}
+
 
 
 void Reset()
@@ -289,6 +435,7 @@ void Reset()
     magia = 0;
     puntos_de_locura = 0;
     puntos_de_destino = 0;
+    nombre = "";
 
     elfo_habilidades.Clear();
     elfo_talentos.Clear();
@@ -309,15 +456,17 @@ while (true)
             Reset();
             Console.WriteLine("Bienvenido al desarrollador de personajes de Warhammer segunda edicion\n" +
                 "Con que raza desea hacer el personaje?\n" +
-                "\nescriba elfo para crear un elfo y enter\n" +
-                "escriba enano para crear un enano y enter\n" +
-                "escriba halfling para crear un halfling y enter\n" +
-                "escriba humano para crear un humano y enter\n" +
+                "\nEscriba elfo para crear un elfo y enter\n" +
+                "Escriba enano para crear un enano y enter\n" +
+                "Escriba halfling para crear un halfling y enter\n" +
+                "Escriba humano para crear un humano y enter\n" +
+                "Si desea un personaje totalmente aleatorio escriba aleatorio y enter" +
                 "Si desea salir escriba salir y enter\n");
             switchControl = Console.ReadLine();
             break;
 
         case "elfo":
+            GeneradorElfo();
             break;
 
         case "enano":
@@ -332,12 +481,12 @@ while (true)
         case "aleatorio":
             break;
 
-        case "exit":
+        case "salir":
             Environment.Exit(0);
             break;
 
         default:
-            Console.WriteLine("Disculpe no reconozco lo que quiso decir >>>> VOLVIENDO AL MENU\n");
+            Console.WriteLine("Disculpe a ocurrido un error >>>> VOLVIENDO AL MENU\n");
             switchControl = "menu";
             break;
     }
